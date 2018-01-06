9fc4750749867050d81100b6c1ed26b6ed716076
Rimmer <m.jelenek@post.cz>
2018-01-06 11:22:07 +0100

    SERIAL_TX_BUFFER_SIZE set to 128 bytes

---
+++ options.h
@@ -0,0 +1,19 @@
+/*
+(1) in the file platform.txt change the following line:
+        build.extra_flags=
+to:
+        build.extra_flags=-I "{build.path}" -include options.h
+
+(2) in the Arduino\hardware\arduino\avr\cores\arduino directory
+create an empty file called options.h
+
+now in any sketch directory you can create a file called options.h
+that contains the things you want to change locally to that sketch.
+in my case this is:
+#define SERIAL_TX_BUFFER_SIZE 64
+#define SERIAL_RX_BUFFER_SIZE 1024
+*/
+
+
+#define SERIAL_TX_BUFFER_SIZE 128
+
