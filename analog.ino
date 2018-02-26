7acce9665b3ca65827b9fd344962b15c55a867ad
Rimmer <m.jelenek@post.cz>
2018-02-26 14:57:20 +0100

    ADC reading optimization

--- analog.ino
+++ analog.ino
@@ -1,9 +1,11 @@
 // ADC conversion complete interrupt handler
+static byte adcIndexStatic = 0;
 ISR(ADC_vect)
 {
-  static byte adcIndex = 0;
-  static byte channelSelect = 0;
-
+  // copy these to local variables so they can be stored in registers
+  // (volatile variables must be read from memory on every access)
+  byte adcIndex = adcIndexStatic;
+ 
   // Read the AD conversion result
   uint16_t sensorValue = ADC;
   
@@ -13,51 +15,50 @@ ISR(ADC_vect)
       break;
     case 1:
       sensorValue0Averaged = ((sensorValue0Averaged + sensorValue) >> 1);
-      channelSelect = 1;
+      ADMUX = 1;                    // select next ADC channel, VREF is EXTERNAL
       break;
     case 2:
       break;
     case 3:
       sensorValue1Averaged = ((sensorValue1Averaged + sensorValue) >> 1);
-      channelSelect = 2;
+      ADMUX = 2;                    // select next ADC channel, VREF is EXTERNAL
       break;
     case 4:
       break;
     case 5:
       sensorValue2Averaged = ((sensorValue2Averaged + sensorValue) >> 1);
-      channelSelect = 3;
+      ADMUX = 3;                    // select next ADC channel, VREF is EXTERNAL
       break;
     case 6:
       break;
     case 7:
       sensorValue3Averaged = ((sensorValue3Averaged + sensorValue) >> 1);
-      channelSelect = 4;
+      ADMUX = 4;                    // select next ADC channel, VREF is EXTERNAL
       break;
     case 8:
       break;
     case 9:
       sensorValue4Averaged = ((sensorValue4Averaged + sensorValue) >> 1);
-      channelSelect = 6;
+      ADMUX = 6;                    // select next ADC channel, VREF is EXTERNAL
       break;
     case 10:
       break;
     case 11:
       sensorValue6Averaged = ((sensorValue6Averaged + sensorValue) >> 1);
-      channelSelect = 7;
+      ADMUX = 7;                    // select next ADC channel, VREF is EXTERNAL
       break;
     case 12:
       break;
     case 13:
       sensorValue7Averaged = ((sensorValue7Averaged + sensorValue) >> 1);
       adcIndex = -1;
-      channelSelect = 0;
+      ADMUX = 0;                    // select next ADC channel, VREF is EXTERNAL
       break;
     default:
       adcIndex = -1;
   }
   adcIndex++;
-
-  ADMUX = channelSelect;             // select next ADC channel, VREF is EXTERNAL
+  adcIndexStatic = adcIndex;
 
   // slow down ADC conversions
   ADCSRA &= ~(1 << ADEN);            // Disable ADC
