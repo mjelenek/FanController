8b235aab70cd1b98b9c91801224e507c84e859f7
Rimmer <m.jelenek@post.cz>
2018-09-19 12:21:25 +0200

    filter RPM signal

--- rpm.ino
+++ rpm.ino
@@ -1,4 +1,6 @@
 #define writeLastFanRpmSensorTimeMacro(i) writeLastFanRpmSensorTime(&lastFanRpmSensorTime[i], fanRpmSensorTimes[i], now);
+// max 15000rpm
+#define MINIMAL_DELAY_BETWEEN_RPM_SIGNAL_CHANGES 1000
 
 #if HWversion == 1
   #include "ISR_Nano.h"
@@ -8,13 +10,16 @@
 #endif
 
 inline __attribute__((always_inline)) void writeLastFanRpmSensorTime(byte *lastFanRpmSensorTime, unsigned long fanRpmSensorTimes[], unsigned long now){
-  byte lastFanRpmSensorTimeValue = *lastFanRpmSensorTime;
-  lastFanRpmSensorTimeValue++;
-  if(lastFanRpmSensorTimeValue >= FAN_RPM_SENSOR_TIMES_FIELD){
-    lastFanRpmSensorTimeValue = 0;
+  byte lastFanRpmSensorTimeIndex = *lastFanRpmSensorTime;
+  unsigned long lastRecord = fanRpmSensorTimes[lastFanRpmSensorTimeIndex];
+  if(now - lastRecord >= MINIMAL_DELAY_BETWEEN_RPM_SIGNAL_CHANGES){
+    lastFanRpmSensorTimeIndex++;
+    if(lastFanRpmSensorTimeIndex >= FAN_RPM_SENSOR_TIMES_FIELD){
+      lastFanRpmSensorTimeIndex = 0;
+    }
+    fanRpmSensorTimes[lastFanRpmSensorTimeIndex] = now;
+    *lastFanRpmSensorTime = lastFanRpmSensorTimeIndex;
   }
-  fanRpmSensorTimes[lastFanRpmSensorTimeValue] = now;
-  *lastFanRpmSensorTime = lastFanRpmSensorTimeValue;
 }
 
 double countRPM(byte lastFanRpmSensorTimeIndex, unsigned long fanRpmSensorTimes[]){
