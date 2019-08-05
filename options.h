d2db90fb6cac61daf028de9635434088174f3f54
Rimmer <m.jelenek@post.cz>
2019-08-05 09:33:00 +0200

    arduino nano memory optimization, set TX_BUFFER_SIZE to 256

--- options.h
+++ options.h
@@ -18,7 +18,7 @@ in my case this is:
   #define SERIAL_TX_BUFFER_SIZE 1024
   #define SERIAL_RX_BUFFER_SIZE 256
 #else
-  #define SERIAL_TX_BUFFER_SIZE 128
+  #define SERIAL_TX_BUFFER_SIZE 256
   #define SERIAL_RX_BUFFER_SIZE 128
 #endif
 
